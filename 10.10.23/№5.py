8139bb17b3f2a72fcd049b54977800a3aaeeebb1
kseniahil <k.kapitonenko06@mail.ru>
2023-10-15 21:59:40 +0300

    5 10.10.23

---
+++ 10.10.23/№5.py
@@ -0,0 +1,6 @@
+numbers = input('Enter three numbers separated by spaces: ')
+sides = list(map(float, numbers.split()))
+p = (sides[0] + sides[1] + sides[2]) / 2
+area = round((p * (p - sides[0]) * (p - sides[1]) * (p - sides[2])) ** (1/2), 3)
+print(area)
+
